94ca8d836911a8126c54f3d73fa6a3b0aca54ba8
itselaineZ <1053643092@qq.com>
2022-03-29 11:00:02 +0800

    begin

---
+++ DiskDriver.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+#include <file>
+using namespace std;
+
+class DiskDriver {
+public:
+    DiskDriver();
+    ~DiskDriver();
+    void Initialize();  //  初始化磁盘镜像
+    void IO(Buf *bp);   //  根据IO请求块进行读写
+
+private:
+    static const char *DISK_FILE_NAME;  //  磁盘镜像文件名
+    static const int BLOCK_SIZE = 512;  //  数据块大小为512字节
+    FILE *fp;   //  磁盘镜像文件指针
+};
